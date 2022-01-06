1f2c49cb7df31aaf30c6d8cf0a1c31dad1e5060f
achristocrat <91918473+achristocrat@users.noreply.github.com>
2022-01-06 18:38:00 -0500

    update1

--- dashboard.py
+++ dashboard.py
@@ -20,26 +20,31 @@ class Crypto:
         resp = requests.request("GET", base + exchange + ticks)
         return resp.json()
 
-        #update: please use klines api to get hist data :)
-    def hist(self, tick = "BTCUSDT", backlog = 5):
-        old = "api/v3/trades"
-        symbol = "?symbol=" + tick
-        limit = "limit=" + str(backlog)
-        st = "start"
-        resp = requests.request("GET", base + old + symbol + "&" + limit)
-        return resp.json()
-
-    def hist_new(self, tick = "BTCUSDT", interval = '1d',  st = np.datetime64("now") - np.timedelta64(30, 'D'), et = np.datetime64("now")):
+    def curr_price(self, tick = "BTCUSDT"):
+        order = {}
+        order["symbol"] = tick
+        resp = requests.request("GET", base + "api/v3/avgPrice", params = order)
+        print(resp.json())
+        return round(float(resp.json()["price"]),2)
+    def hist_new(self, tick = "BTCUSDT", tr = '1d'):
         order = {}
         old = "api/v3/klines"
+        points = {"1d": ["1m", np.timedelta64(1, 'D')],
+                "1w": ["1h", np.timedelta64(7, 'D')],
+                "1m": ["4h", np.timedelta64(30, 'D')],
+                "3m": ["6h", np.timedelta64(90, 'D')],
+                "1y": ["1w", np.timedelta64(365, 'D')],
+                "3y": ["1w", np.timedelta64(1095, 'D')]
+                }
         order["symbol"] = tick
-        order["interval"] = interval
-        order["startTime"] = str(self.to_ms(st))
-        order["endTime"] = str(self.to_ms(et))
+        order["interval"] = points[tr][0]
+        now = np.datetime64("now")
+        order["startTime"] = str(self.to_ms(now-points[tr][1]))
+        order["endTime"] = str(self.to_ms(now))
         resp = requests.request("GET", base + old, params= order)
         his = pd.DataFrame(resp.json(), columns=["opentime", "open", "high", "low", "close", "volume", "closetime", "qav", "ntrades", "tbav", "tqav", "ig"])
         his = his.astype(float)
-        his["timestamps"] = pd.date_range(st, et, len(his))
+        his["timestamps"] = pd.date_range(now-points[tr][1], now, len(his))
         return his
     
     def to_ts(self, ms):
@@ -59,7 +64,7 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
-app = dash.Dash(external_stylesheets=[dbc.themes.YETI, "https://codepen.io/chriddyp/pen/bWLwgP.css"])
+app = dash.Dash(external_stylesheets=[dbc.themes.YETI, "https://codepen.io/chriddyp/pen/bWLwgP.css", "https://www.herokucdn.com/purple3/latest/purple3.min.css"])
 cp = Crypto()
 df = cp.hist_new()
 
@@ -71,8 +76,7 @@ app.layout=html.Div([
         dbc.Col(
             dbc.Accordion([
                 dbc.AccordionItem([
-                    dcc.Input(placeholder="Search by Tick...", value=""),
-                    html.Button("Search")
+                    dcc.Input(placeholder="Search by Tick...", value="BTCUSDT", id="search", size="8")
                 ],
                 title="Search"),
                 dbc.AccordionItem([
@@ -84,7 +88,7 @@ app.layout=html.Div([
                             {'label': 'Close', 'value': 'close'},
                             {'label': 'Volume', 'value': 'volume'}
                         ],
-                        value='High',
+                        value='high',
                         id='ddohlc'
                     )
                 ],
@@ -97,16 +101,18 @@ app.layout=html.Div([
                             {'label': '1 Week', 'value': '1w'},
                             {'label': '1 Month', 'value': '1m'},
                             {'label': '3 Months', 'value': '3m'},
-                            {'label': '1 Year', 'value': '1y'}
+                            {'label': '1 Year', 'value': '1y'},
+                            {'label': '3 Years', 'value': '3y'}
                         ],
-                        value='1 Month',
+                        value='1d',
                         id='times'
                     )
                 ],
                 title="Time Range"
                 ),
 
-            ]),
+            ],
+            flush=True),
             width=2
         ),
         dbc.Col(
@@ -122,16 +128,55 @@ app.layout=html.Div([
     ]),
     dbc.Row([
         dbc.Col([
-            html.H1("Today's Statistics", className='display-3'),
-            html.Hr(className='my-2'),
-            html.P("High: "+ "Low: "+ "Open: "+ "Close: ")
+            dbc.Container(
+        [
+            html.H1("Live Value Counter", className="display-3"),
+            html.H2("Tick here",
+                className="lead",
+                id="jtron"
+            ),
+            html.Hr(className="my-2"),
+            html.P("Refreshed Cryptovalue", id="cval"),
+            dcc.Interval(interval=30000, n_intervals=0, id="internal-component")
         ],
-        className="h-100 p-5 text-white border bg-dark rounded-3",
+        fluid=True,
+        className="py-3",
+        )
+        ],
         width={"offset":2}),
-        
+        dbc.Col([
+            html.H1("Similar Crypto", className='display-3'),
+            html.Hr(className='my-2'),
+            html.P("Coming Soon!")
+        ],
+        className="h-100 p-5 text-white border bg-dark rounded-3"),
+
     ]),
     dbc.Row(html.P("Made by Christopher Pak, 2022"))
 ])
+
+@app.callback(
+    [Output("mainplot", "figure"),
+    Output("volplot", "figure"),
+    Output("jtron", "children")],
+    [Input("search", "value"),
+    Input("ddohlc", "value"),
+    Input("times", "value")]
+)
+def update_figs(query, opt, interval):
+    cp = Crypto()
+    new_df = cp.hist_new(query.upper(), interval)
+    fig1 = px.line(new_df, x="timestamps", y=opt, title= (query + " Price Timeline"))
+    fig2 = px.bar(new_df,x="timestamps",y="volume", title=(query + " Volume"))
+    return fig1, fig2, query
+@app.callback(
+    Output("cval", "children"),
+    [Input("internal-component", "n_intervals"),
+    Input('search', 'value')]
+)
+def live_price(n, query):
+    cp = Crypto()
+    return cp.curr_price(tick=query)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
